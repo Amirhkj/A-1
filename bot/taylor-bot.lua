ba6fc800dcf1e03233eff1ad227e03072e10f61f
githubtelebot <github.telebot@gmail.com>
2016-06-20 13:37:55 +0200

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -233,23 +233,23 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {68747297},--Sudo users
+    sudo_users = {102117869},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Taylor Team v4
     Taylor Team and Taylor Bot Anti spam / anti link
     
-    website : 
-    taylortg.ir  ❤️
+    name bot : 
+    Tayler
     
     admin : 
     
-    @negative_officiall ❤️
+    @Amirrezajeddi_Admin ❤️
     
     
     channel : 
     
     
-    @taylor_Team ❤️
+    @taylorchanel ❤️
 ]],
     help_text_realm = [[
 Realm Commands:
